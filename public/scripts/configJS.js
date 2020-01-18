02dc89a03ba0a4a0614a6c676a098bc4d38ecb55
Hien Nguyen <biro@Hiens-MacBook-Air.local>
2020-01-17 21:13:58 -0800

    Fixed bugs

--- public/scripts/configJS.js
+++ public/scripts/configJS.js
@@ -1,2 +1,2 @@
 var API_SERVER = 'http://vnsdev.com/gp-map/api/'; // Ex. Your API will be 'http://192.168.64.2/gp-map/api/user/read.php'
-var API_KEY_GOOGLE_MAP = 'AIzaSyA8lt4Q_nxQKL7isuoL_Fwm8PBkmEXvMBo'; 
+var API_KEY_GOOGLE_MAP = 'API_______'; 
